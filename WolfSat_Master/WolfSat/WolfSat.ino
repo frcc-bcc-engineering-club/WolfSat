6364654f2f92261e5edb44a062113aafe25b55b4
FRCCSour137 <47700245+FRCCSour137@users.noreply.github.com>
2019-02-16 16:38:39 -0700

    WolfTest Driver
    Use this driver to test your classes.

---
+++ WolfSat_Master/WolfSat/WolfSat.ino
@@ -0,0 +1,15 @@
+#include <wolf.h>
+wolf master;
+
+#include <SPI.h>
+
+void setup() {
+  master = wolf();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
